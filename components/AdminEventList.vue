d5dd3a646f99c5e97d71fc4b7d391f534f9946a4
Jens Krieger-Juhnke <kjd@kirchenkreis-burgdorf.de>
2024-03-28 23:27:33 +0000

    fix: icons in AdminEventList are matching user view now

--- components/AdminEventList.vue
+++ components/AdminEventList.vue
@@ -22,6 +22,24 @@
           </div>
         </td>
         <td>
+          <div class="badge badge-md badge-accent gap-2 badge-outline">
+            
+            <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            class="w-5 h-5"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M5.5 3A2.5 2.5 0 003 5.5v2.879a2.5 2.5 0 00.732 1.767l6.5 6.5a2.5 2.5 0 003.536 0l2.878-2.878a2.5 2.5 0 000-3.536l-6.5-6.5A2.5 2.5 0 008.38 3H5.5zM6 7a1 1 0 100-2 1 1 0 000 2z"
+              clip-rule="evenodd"
+            />
+          </svg>
+            {{ event.expand.category.name }}
+          </div>
+        </td>
+        <td>
           <div class="badge badge-md badge-info gap-2 badge-outline">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -35,24 +53,7 @@
                 clip-rule="evenodd"
               />
             </svg>
-
-            {{ event.expand.category.name }}
-          </div>
-        </td>
-        <td>
-          <div class="badge badge-md badge-accent gap-2 badge-outline">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              class="w-5 h-5"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M5.5 3A2.5 2.5 0 003 5.5v2.879a2.5 2.5 0 00.732 1.767l6.5 6.5a2.5 2.5 0 003.536 0l2.878-2.878a2.5 2.5 0 000-3.536l-6.5-6.5A2.5 2.5 0 008.38 3H5.5zM6 7a1 1 0 100-2 1 1 0 000 2z"
-                clip-rule="evenodd"
-              />
-            </svg>
+            
 
             {{ event.expand.location.name }}
           </div>
