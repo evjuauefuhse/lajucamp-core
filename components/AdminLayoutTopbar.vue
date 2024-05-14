fbf7d3462183e239d08968430feafe747472c89a
Jens Krieger-Juhnke <kjd@kirchenkreis-burgdorf.de>
2024-05-14 22:24:41 +0000

    fix: layout in admin view

--- components/AdminLayoutTopbar.vue
+++ components/AdminLayoutTopbar.vue
@@ -22,7 +22,7 @@
     </div>
     <div class="drawer-side">
       <label for="my-drawer-2" aria-label="close sidebar" class="drawer-overlay"></label>
-      <ul class="menu menu-lg p-4 w-80 min-h-full bg-base-200 text-base-content flex flex-col justify-between">
+      <ul class="menu menu-lg w-80 min-h-full bg-base-200 text-base-content flex flex-col justify-between">
         <div>
           <!-- Sidebar content here -->
           <AdminSidebarUser />
@@ -49,7 +49,7 @@
 
 
               Aufgaben <span v-show="Object.keys(taskStore.tasks).length !== 0" class="badge badge-error">{{
-              Object.keys(taskStore.tasks).length }}</span></RouterLink>
+                Object.keys(taskStore.tasks).length }}</span></RouterLink>
           </li>
           <li>
             <RouterLink :class="checkActiveMenu('admin-events')" to="/admin/events"><svg
@@ -101,6 +101,19 @@
               </svg>
 
               Willkommensnachrichten</RouterLink>
+          </li>
+          <li v-if="pb.authStore.model.managePages">
+            <RouterLink :class="checkActiveMenu('admin-pages')" to="/admin/pages"><svg
+                xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                <path fill-rule="evenodd"
+                  d="M5.625 1.5c-1.036 0-1.875.84-1.875 1.875v17.25c0 1.035.84 1.875 1.875 1.875h12.75c1.035 0 1.875-.84 1.875-1.875V12.75A3.75 3.75 0 0 0 16.5 9h-1.875a1.875 1.875 0 0 1-1.875-1.875V5.25A3.75 3.75 0 0 0 9 1.5H5.625ZM7.5 15a.75.75 0 0 1 .75-.75h7.5a.75.75 0 0 1 0 1.5h-7.5A.75.75 0 0 1 7.5 15Zm.75 2.25a.75.75 0 0 0 0 1.5H12a.75.75 0 0 0 0-1.5H8.25Z"
+                  clip-rule="evenodd" />
+                <path
+                  d="M12.971 1.816A5.23 5.23 0 0 1 14.25 5.25v1.875c0 .207.168.375.375.375H16.5a5.23 5.23 0 0 1 3.434 1.279 9.768 9.768 0 0 0-6.963-6.963Z" />
+              </svg>
+
+
+              Seiten</RouterLink>
           </li>
           <li v-if="pb.authStore.model.manageTeams">
             <RouterLink :class="checkActiveMenu('admin-teams')" to="/admin/teams">
@@ -187,17 +200,19 @@ function isSameDate(date1, date2) {
     date1.getDate() === date2.getDate()
   );
 }
-
-let payload = {};
-if (!pb.authStore.model.manageAllEvents) {
-  payload.filter = 'team="' + pb.authStore.model.team + '"';
-}
-
-let events = await pb.collection("events").getFullList(payload);
-
-events.forEach(event => {
-  if (!isSameDate(new Date(event.start), new Date(event.end))) {
-    taskStore.add(event.id, "SPANS_MULTIPLE_DAYS")
+onMounted(async () => {
+  let payload = {};
+  if (!pb.authStore.model.manageAllEvents) {
+    payload.filter = 'team="' + pb.authStore.model.team + '"';
   }
+
+  let events = await pb.collection("events").getFullList(payload);
+
+  events.forEach(event => {
+    if (!isSameDate(new Date(event.start), new Date(event.end))) {
+      taskStore.add(event.id, "SPANS_MULTIPLE_DAYS")
+    }
+  })
 })
+
 </script>
