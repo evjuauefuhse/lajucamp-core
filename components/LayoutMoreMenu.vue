a875c5d781659cb3a56bf3b640162d25be81d772
Jens Krieger-Juhnke <kjd@kirchenkreis-burgdorf.de>
2024-07-22 16:46:07 +0000

    feat: move more menu to bottom drawer

---
+++ components/LayoutMoreMenu.vue
@@ -0,0 +1,91 @@
+<template>
+    <div class="modal-box bg-base-200">
+        <form method="dialog">
+            <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+        </form>
+        <h3 class="text-xl font-bold mb-3">Mehr</h3>
+        <ul class="menu menu-lg p-0">
+            <!--<MenuEntry @click="closeMoreDialog" v-if="checkDevelAccess()" name="Entwicklerinformationen" to="/test">
+                <path stroke-linecap="round" stroke-linejoin="round"
+                    d="M17.25 6.75L22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3l-4.5 16.5" />
+            </MenuEntry>
+            <MenuEntry @click="closeMoreDialog" v-if="checkPictureAccess()" name="Bildergalerie" to="/pictures">
+                <path stroke-linecap="round" stroke-linejoin="round"
+                    d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
+            
+            </MenuEntry>-->
+            <div v-if="records.length !== 0">
+                <div v-for="record in records" :key="record.id">
+                    <MenuEntry @click="closeMoreDialog" v-if="checkAccessKey(record)" :name="record.name"
+                        :to="(record.redirectUrl !== '') ? record.redirectUrl : '/misc/page-' + record.id">
+                        <div v-html="record.icon"></div>
+                    </MenuEntry>
+                </div>
+            </div>
+
+
+
+        </ul>
+    </div>
+</template>
+
+<script setup>
+const config = useRuntimeConfig()
+
+const cookie = useCookie("keys", { expires: new Date('9999-12-31') })
+
+function hackyCookieWorkaround() {
+    const currentType = typeof cookie.value
+    if (currentType === "string") {
+        return JSON.parse(cookie.value)
+    } else {
+        return cookie.value
+    }
+}
+
+function checkMusicAccess() {
+    if (cookie.value === undefined) return false
+    const data = hackyCookieWorkaround()
+    return data.some(obj => obj.allowMusic === true)
+}
+
+const pageManager = usePageManager()
+
+let records = ref([])
+
+onMounted(async () => {
+    records.value = await pageManager.getList()
+})
+
+
+function checkDevelAccess() {
+    if (cookie.value === undefined) return false
+    const data = hackyCookieWorkaround()
+    return data.some(obj => obj.enableDevelResources === true)
+}
+
+function checkPictureAccess() {
+    if (cookie.value === undefined) return false
+    const data = hackyCookieWorkaround()
+    return data.some(obj => obj.allowImages === true)
+}
+
+function checkAccessKey(record) {
+    if (!record.needsFeatureKey) {
+        console.log("Allow because no featureKey set")
+        return true
+    }
+    else {
+        if (cookie === undefined || cookie.value === undefined) {
+            console.log("Deny because no cookie set")
+            return false
+        }
+        else {
+            console.log("Check if cookie contains featureKey")
+            return cookie.value.some(obj => obj.id === record.featureKey)
+        }
+    }
+
+}
+
+</script>
