4b6ea5c109c236a687b44a42a1dfe4dff18dd578
Jens Krieger-Juhnke <kjd@kirchenkreis-burgdorf.de>
2024-08-07 15:04:50 +0000

    fix: center text in add instance dialog

--- pages/misc/settings/instances/add.vue
+++ pages/misc/settings/instances/add.vue
@@ -15,7 +15,7 @@
                             <div class="flex flex-row justify-center w-full pt-8">
                                 <span class="text-3xl font-black">Willkommen bei der</span>
                             </div>
-                            <div class="flex flex-row justify-center w-full">
+                            <div class="flex flex-row justify-center text-center w-full">
                                 <span class="inline-grid"><span
                                         class="text-3xl font-black pointer-events-none col-start-1 row-start-1 bg-[linear-gradient(90deg,theme(colors.error)_0%,theme(colors.secondary)_9%,theme(colors.secondary)_42%,theme(colors.primary)_47%,theme(colors.accent)_100%)] bg-clip-text blur-xl [transform:translate3d(0,0,0)] [-webkit-text-fill-color:transparent] before:content-[attr(data-text)] [@supports(color:oklch(0_0_0))]:bg-[linear-gradient(90deg,oklch(var(--s))_4%,color-mix(in_oklch,oklch(var(--s)),oklch(var(--er)))_22%,oklch(var(--p))_45%,color-mix(in_oklch,oklch(var(--p)),oklch(var(--a)))_67%,oklch(var(--a))_100.2%)]"
                                         aria-hidden="true" data-text="Evangelischen Jugend"></span><span
