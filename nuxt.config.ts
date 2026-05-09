1395848c82da71a9547b9f5759dde0c3fbb24b1f
Jens <jens@evju-auefuhse.de>
2026-05-09 14:21:44 +0200

    feat: update nuxt version

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  //ssr: false,
+  ssr: false,
+  experimental: {
+    viteEnvironmentApi: true,
+  },
   modules: ['@nuxtjs/tailwindcss', '@kevinmarrec/nuxt-pwa', '@nuxtjs/device', 'nuxt-swiper', '@pinia/nuxt'],
   tailwindcss: {
     config: {
