edc97858e628299d2be4b760f5ed1071cae25d48
Jens <jens@evju-auefuhse.de>
2026-05-09 14:30:20 +0200

    feat: initial work for capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,25 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'de.ejh.app',
+  appName: 'Evangelische Jugend',
+  webDir: '.output/public'
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 2000,
+      launchAutoHide: true,
+      androidScaleType: 'CENTER_CROP',
+      splashFullScreen: true,
+      splashImmersive: true,
+    },
+    Keyboard: {
+      resize: 'body',
+      resizeOnFullScreen: true,
+    },
+    StatusBar: {
+      style: 'dark',
+    },
+  },
+};
+
+export default config;
