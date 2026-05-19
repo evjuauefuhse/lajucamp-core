eb52c96980a337573ee80893e1b9a7ce817be358
Jens <jens@evju-auefuhse.de>
2026-05-19 15:19:14 +0200

    hotfix: remove 'back to scanner' button

--- pages/misc/settings/keys/add.vue
+++ pages/misc/settings/keys/add.vue
@@ -24,7 +24,7 @@
                 <input v-model="key" type="text" placeholder="Berechtigungsschlüssel eingeben"
                     class="input input-bordered w-full" />
                 <div class="flex flex-row justify-end py-4">
-                    <div class="btn btn-ghost" @click="qr = true">Zurück zum Scanner</div>
+                    <div v-if="0" class="btn btn-ghost" @click="qr = true">Zurück zum Scanner</div>
                     <div :disabled="(key === '') ? 1 : False" class="btn btn-primary" @click="validate">Weiter</div>
                 </div>
             </div>
