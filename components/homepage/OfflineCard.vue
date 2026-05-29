31d824bcaf887cd263df30c555402b110cf4db87
Jens <jens@evju-auefuhse.de>
2026-05-29 15:17:13 +0200

    feat: add icon to offline card

--- components/homepage/OfflineCard.vue
+++ components/homepage/OfflineCard.vue
@@ -1,7 +1,13 @@
 <template>
-    <div class="card card-compact bg-error text-primary-content shadow-xl">
+    <div class="card bg-error text-primary-content shadow-xl">
         <div class="card-body">
-            <h2 class="card-title">Du bist offline.</h2>
+            <h2 class="card-title">
+		    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m9-.75a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 3.75h.008v.008H12v-.008Z" />
+</svg>
+
+Du bist offline.
+	    </h2>
             <p>Einige Inhalte werden möglicherweise nicht korrekt dargestellt, bis du wieder online bist.</p>
         </div>
     </div>
